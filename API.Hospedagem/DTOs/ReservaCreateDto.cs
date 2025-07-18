a29cbe7893c33421ad4b788ea2cc4c24b838068e
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-07-17 23:43:07 -0300

    feat-DTO_Create_Reserva

--- API.Hospedagem/DTOs/ReservaCreateDto.cs
+++ API.Hospedagem/DTOs/ReservaCreateDto.cs
@@ -1,8 +1,22 @@
-﻿namespace API.Hospedagem.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Hospedagem.DTOs
 {
     public class ReservaCreateDto
     {
         // Dados de entrada para criar reserva 
+
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public int numero { get; set; }
+
+        [Required]
+        public DateTime dataCheckin { get; set; }
+
+
+
 
 
     }
