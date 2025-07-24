c129fc98ef2debd28d3331aae7f10ac1fb96618a
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-07-24 16:03:49 -0300

    feat-construcao_estrutura_Funcionario_Cargo

---
+++ API.Hospedagem/Models/Cargo.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Hospedagem.Models
+{
+    public class Cargo
+    {
+
+        public int Id { get; set;  }
+        public string Nome { get; set; } = null!;
+
+
+        public ICollection<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+
+
+    }
+}
---
+++ API.Hospedagem/Models/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿namespace API.Hospedagem.Models
+{
+    public class Funcionario
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = null!;
+
+        public string CPF { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Telefone { get; set; } = null!;
+
+        public string Endereco { get; set; } = null!;   
+
+
+
+        // Defiindo a FK do Cargo 
+        public int CargoId { get; set; }
+        public Cargo cargo { get; set; } = null!;
+
+
+
+    }
+}
