546337cb3f9cfbd5b15a3903d0fb70a2fc317382
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-09-29 20:34:27 -0300

    test-get_All_Method

--- API.Hospedagem.Tests/TestesControllers/FuncionarioControllerTest.cs
+++ API.Hospedagem.Tests/TestesControllers/FuncionarioControllerTest.cs
@@ -57,9 +57,9 @@ namespace API.Hospedagem.Tests.TestesControllers
             var funcionario = new FuncionarioReadDto
             {
 
-           
 
-                Id = 6,
+
+                Id = 4,
                 Nome = "Jill Valentine",
                 CPF = "12345678900",
                 Email = "jill@hotel.com",
@@ -73,13 +73,13 @@ namespace API.Hospedagem.Tests.TestesControllers
 
 
             // ARRANGE
-            _serviceMock.Setup(s => s.GetByIdAsync(6)).ReturnsAsync(funcionario);
+            _serviceMock.Setup(s => s.GetByIdAsync(4)).ReturnsAsync(funcionario);
 
 
 
             // ACT
 
-            var result = await _controller.GetById(6);
+            var result = await _controller.GetById(4);
 
             // Passando o ID que foi mockado
 
@@ -92,7 +92,110 @@ namespace API.Hospedagem.Tests.TestesControllers
 
 
 
-            _serviceMock.Verify(s => s.GetByIdAsync(6), Times.Once);
+            _serviceMock.Verify(s => s.GetByIdAsync(4), Times.Once);
         }
-    }
+
+
+
+
+
+
+
+        [Fact]
+        public async Task GetAll_Deve_Retornar_Ok_Com_Lista_Funcionarios() {
+
+            #region ARRANGE
+            var esperados = new List<FuncionarioReadDto> {
+
+
+
+                new(){
+                     Id = 6, Nome = "Jill Valentine", CPF = "12345678900",
+            Email = "jill@hotel.com", Telefone = "91234-5678",
+            Endereco = "Rua A, 1443", CargoId = 2, CargoNome = "Serviços Gerais"
+                   },
+
+         
+                //new()
+                //{
+                //     Id = 5, Nome = "João Costa", CPF = "12345678911",
+                //     Email = "joao@hotel.com", Telefone = "91234-6666",
+                //    Endereco = "Rua C, 1455", CargoId = 1, CargoNome = "Gestão"
+
+                //}
+
+        };
+
+
+
+            _serviceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(esperados);
+
+            #endregion
+
+
+
+            #region ACT
+            var result = await _controller.GetAll();
+            #endregion
+
+
+            #region ASSERT -> OK + Content
+            var ok = result.Result.Should().BeOfType<OkObjectResult>().Which;
+            var lista = ok.Value.Should().BeAssignableTo<IEnumerable<FuncionarioReadDto>>().Which;
+            #endregion
+
+            _serviceMock.Verify(s => s.GetAllAsync(), Times.Once); 
+
+
+
+
+        }
+
+        [Fact]
+        public async Task GetAll_quando_vazio_retorna_Ok_com_lista_vazia() {
+
+            // ARRANGE
+            _serviceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(new List<FuncionarioReadDto>()); // Simula o retorno vazio do serviço
+
+
+            // ACT
+           var result = await _controller.GetAll(); // So verifica se o resultado é http eh o esperado
+
+
+
+           // // ASSERT
+
+            result.Result.Should().BeOfType<OkObjectResult>(); // Verifica se o resultado é um OkObjectResult
+            var ok = result.Result as OkObjectResult; // Converte o resultado para OkObjectResult
+            ok.Should().NotBeNull(); // Verifica se ok não é nulo
+
+
+
+            var lista = ok!.Value as IEnumerable<FuncionarioReadDto>; // Verifica se o valor retornado é uma lista de FuncionarioReadDto
+            lista.Should().BeNullOrEmpty();// Verifica se a lista é nula ou vazia
+            lista!.Should().BeEmpty(); // Verifica se a lista tem 0 elementos
+
+
+            _serviceMock.Verify(s => s.GetAllAsync(), Times.Once);// Verifica se o método foi chamado exatamente uma vez
+
+
+
+
+
+
+        }
+
+
+
+
+        // TODO: Implementar os testes restantes ( Create, Update, Delete)
+
+        [Fact]
+        public async Task Create_Deve_Retornar_CreatedAtRoute_Com_Funcionario_Criado()
+        {
+
+            //_serviceMock.Setup(s => s.CreateAsync()).
+
+
+        }
 }
