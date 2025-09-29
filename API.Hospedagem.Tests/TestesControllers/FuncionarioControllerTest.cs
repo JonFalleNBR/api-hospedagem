0057102ed63469f62ee4d804327db1f1374531ab
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-09-29 01:31:43 -0300

    test-teste_validacao_campo_encontrado

--- API.Hospedagem.Tests/TestesControllers/FuncionarioControllerTest.cs
+++ API.Hospedagem.Tests/TestesControllers/FuncionarioControllerTest.cs
@@ -49,5 +49,50 @@ namespace API.Hospedagem.Tests.TestesControllers
         }
 
 
+        [Fact]
+        public async Task GetById_Quando_existe_retorna_Ok() {
+
+
+            // ARRANGE -> Mockando explicitamente um funcionário 
+            var funcionario = new FuncionarioReadDto
+            {
+
+           
+
+                Id = 6,
+                Nome = "Jill Valentine",
+                CPF = "12345678900",
+                Email = "jill@hotel.com",
+                Telefone = "91234-5678",
+                Endereco = "Rua A, 1443",
+                CargoId = 2,
+                CargoNome = "Serviços Gerais"
+
+
+            };
+
+
+            // ARRANGE
+            _serviceMock.Setup(s => s.GetByIdAsync(6)).ReturnsAsync(funcionario);
+
+
+
+            // ACT
+
+            var result = await _controller.GetById(6);
+
+            // Passando o ID que foi mockado
+
+
+            // ASSERT
+
+            result.Result.Should().BeOfType<OkObjectResult>(); // Verifica se o resultado é um OkObjectResult
+            var ok = result.Result as OkObjectResult; // Converte o resultado para OkObjectResult
+            ok!.Value.Should().BeEquivalentTo(funcionario); // Verifica se o valor retornado é equivalente ao funcionário mockado
+
+
+
+            _serviceMock.Verify(s => s.GetByIdAsync(6), Times.Once);
+        }
     }
 }
