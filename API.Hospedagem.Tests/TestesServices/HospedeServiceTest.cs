0bb06256235725ba6361c71a48b7f039648d3855
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-10-08 23:34:49 -0300

    test-inicio_teste_hospede

---
+++ API.Hospedagem.Tests/TestesServices/HospedeServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using API.Hospedagem.Data;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Hospedagem.Tests.TestesServices
+{
+    public class HospedeServiceTest
+    {
+
+
+        private static ApplicationDbContext NewCtx()
+        {
+
+
+            // declaracao clara de opcoes para o contexto 
+            var opts = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+
+
+            var ctx = new ApplicationDbContext(opts);
+
+
+            //if (!ctx.Cargos.Any())
+            //{
+
+            //    ctx.Cargos.AddRange(
+            //           new Models.Cargo { Id = 1, Nome = "Gerente" },
+            //              new Models.Cargo { Id = 2, Nome = "Recepção" }
+
+
+
+            //        );
+
+            //    ctx.SaveChanges();
+
+            //}
+            return ctx;
+        }
+
+
+        // Cria um novo mapeador  para simular o AutoMapper da Service
+        private static IMapper NewMapper()
+        {
+
+
+
+            var cgf = new MapperConfiguration(cfg =>
+                             cfg.AddProfile<API.Hospedagem.Profiles.MappingProfile>());
+
+            return cgf.CreateMapper();
+
+        }
+
+
+    }
+}
